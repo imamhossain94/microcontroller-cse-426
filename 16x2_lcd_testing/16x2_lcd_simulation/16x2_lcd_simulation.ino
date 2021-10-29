3d8d86554812c2d6b16d52250a14f1da13b6bea2
imamhossain94 <imamagun94@gmail.com>
2021-10-29 10:20:17 +0600

    feat: 16x2_lcd_testing

---
+++ 16x2_lcd_testing/16x2_lcd_simulation/16x2_lcd_simulation.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+#define rs 9 
+#define en 8 
+#define d4 7 
+#define d5 6  
+#define d6 5 
+#define d7 4 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+ 
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Lal Vai");
+}
+ 
+void loop() {
+  lcd.setCursor(3,1); 
+  lcd.print("Seconds"); 
+ 
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
+}
